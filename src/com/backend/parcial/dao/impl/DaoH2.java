e49f318175e526a5fe755878a2b8501db7bf8e24
nrealr <nrealr@gmail.com>
2023-10-25 21:46:29 -0300

    agregando h2connection

--- src/com/backend/parcial/dao/impl/DaoH2.java
+++ src/com/backend/parcial/dao/impl/DaoH2.java
@@ -1,4 +1,19 @@
 package com.backend.parcial.dao.impl;
 
-public class DaoH2 {
+import com.backend.parcial.dao.H2Connection;
+import com.backend.parcial.dao.IDao;
+import org.apache.log4j.Logger;
+
+import java.sql.*;
+
+public class DaoH2 implements IDao {
+
+    private final Logger LOGGER = Logger.getLogger(DaoH2.class);
+
+
+    //trycatch de la interfaz a implementar
+
+   Connection connection = null;
+
+
 }
