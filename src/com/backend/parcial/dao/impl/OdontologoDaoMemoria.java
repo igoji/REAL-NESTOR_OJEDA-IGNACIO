52e593aaa99413ec2d2db2fcb7a5257d8809a7fc
igoji <ignacioantonioo@gmail.com>
2023-10-26 21:39:57 -0300

    agregando OdontologoDaoMemoria

---
+++ src/com/backend/parcial/dao/impl/OdontologoDaoMemoria.java
@@ -0,0 +1,33 @@
+package com.backend.parcial.dao.impl;
+
+import com.backend.parcial.dao.IDao;
+import com.backend.parcial.model.Odontologo;
+import org.apache.log4j.Logger;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OdontologoDaoMemoria implements IDao<Odontologo> {
+
+    private List<Odontologo> listaOdontologos = new ArrayList<>();
+    private final Logger LOGGER = Logger.getLogger(OdontologoDaoMemoria.class);
+
+    public OdontologoDaoMemoria(List<Odontologo> listaOdontologos) {
+        this.listaOdontologos = listaOdontologos;
+    }
+
+    @Override
+    public Odontologo registrar(Odontologo odontologo) {
+        int id = listaOdontologos.size() + 1;
+        listaOdontologos.add(odontologo);
+        Odontologo odontologoGuardado = new Odontologo(id, odontologo.getNumero_matricula(), odontologo.getNombre(), odontologo.getApellido());
+        LOGGER.info("Odontologo guardado: " + odontologoGuardado);
+
+        return odontologoGuardado;
+    }
+
+    @Override
+    public List<Odontologo> listarTodos() {
+        return listaOdontologos;
+    }
+}
