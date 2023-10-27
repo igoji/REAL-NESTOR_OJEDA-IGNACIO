34c9c9e5b6c2bf6129b36d716f47aa4227ee206d
igoji <ignacioantonioo@gmail.com>
2023-10-26 21:48:07 -0300

    agregando OdontologoService

---
+++ src/com/backend/parcial/service/OdontologoService.java
@@ -0,0 +1,25 @@
+package com.backend.parcial.service;
+
+import com.backend.parcial.dao.IDao;
+import com.backend.parcial.model.Odontologo;
+
+import java.util.List;
+
+public class OdontologoService {
+
+    private IDao<Odontologo> OdontologoIDao;
+
+    public OdontologoService(IDao<Odontologo> odontologoIDao) {
+        OdontologoIDao = odontologoIDao;
+    }
+
+    public Odontologo registrarOdontologo(Odontologo odontologo){
+        return OdontologoIDao.registrar(odontologo);
+    }
+
+    public List<Odontologo> listarOdontologos(){
+        return OdontologoIDao.listarTodos();
+    }
+
+
+}
